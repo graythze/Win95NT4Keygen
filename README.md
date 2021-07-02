7075941b6b2673729a98eded293a03906da7c1cb
graythze <54765502+graythze@users.noreply.github.com>
2021-07-02 17:04:47 +0500

    Add arguments for gen keys

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 The first segment is the site number. It can be **from 000 to 998**. Some numbers like **333, 444, 555, 666, 777, 888, 999** aren't allowed
 
-The sum of second segment must be divisible by seven and the last digit cannot be **0 or >= 8**
+The sum of second segment must be divisible by seven, and the last digit cannot be **0 or >= 8**
 
 *****The most basic valid key is 000-0000007*****
 
@@ -25,20 +25,26 @@ The third segment has fully random digits.
 
 The first segment is the site number. It can be from **0001 to 9991**. The last digit much be **3rd digit + 1 or 2**. When the result is **> 9**, it overflows to **0 or 1**.
 
-The sum of second segment must be divisible by seven and the last digit cannot be **0 or >= 8** (identical CD Keys)
+The sum of second segment must be divisible by seven, and the last digit cannot be **0 or >= 8** (identical CD Keys)
 
 *****The most basic valid key is 0001-0000007*****
 
 # Usage
-* Type `python main.py` to launch
+* Use `python generator.py -c` to *generate CD key*
+  
+  Use `python generator.py -o` to *generate OEM key*
 
-* File `keys.txt` will be created. Open it to get keys
+  Use `python generator.py -e` to *generate 11-digit CD key*
 
-# Legal of using this code
-This code was made **only** for educational purposes based on leaked Windows NT 4 source code.
+  Use `python generator.py -c -o -e` to *generate CD, OEM and 11-digit CD key together*
+
+* File `keys<unixDate>.txt` will be created. Open it to get keys
 
 # Sources
 ***Windows NT 4.0 and Windows 95 Key Generator*** by ***[@nilaerdna](https://github.com/nilaerdna/Windows95NT4KeyGenerator)***
 
 ***Examining the Microsoft mod7 product key algorithm*** by ***[@dgurney](https://gurney.dev/posts/mod7/)***
 
+**Leaked Windows NT 4 source code***
+
+**this code was made **only** for educational purposes. Use it at your own risk*
---
+++ generator.py
@@ -0,0 +1,40 @@
+import argparse
+import time
+import cd_key
+import oem_key
+import eleven_cd_key
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Use python generator.py <arg(s)> to generate CD, OEM or 11-digit key')
+    parser.add_argument('-c', '--cdkey',
+                        action='store_true',
+                        help='Generate CD key')
+    parser.add_argument('-o', '--oemkey',
+                        action='store_true',
+                        help='Generate OEM key')
+    parser.add_argument('-e', '--elevencdkey',
+                        action='store_true',
+                        help='Generate 11-digit CD key')
+    args = parser.parse_args()
+
+    now = int(time.time())
+    key_file = open("keys" + str(now) + ".txt", "w")
+
+    if args.cdkey is True:
+        cd_key = cd_key.cd_keygen_first_segment() + '-' + cd_key.check_seven_digit()
+        key_file.write("CD Key: " + cd_key + "\n")
+        print("CD Key has been generated")
+    if args.oemkey is True:
+        oem_key = oem_key.oem_first_segment() + '-OEM-' + oem_key.check_second_digit() + '-' + oem_key.oem_third_segment()
+        key_file.write("OEM Key: " + oem_key + "\n")
+        print("OEM Key has been generated")
+    if args.elevencdkey is True:
+        cd_key_2 = eleven_cd_key.eleven_cd_keygen_first_segment() + '-' + eleven_cd_key.check_seven_digit()
+        key_file.write("11-digit CD Key: " + cd_key_2 + "\n")
+        print("11-digit CD Key has been generated")
+    else:
+        key_file.write("Looks like you've started script w/0 arguments \n"
+                       "Use 'python generator.py -c' to generate CD key \n"
+                       "Use 'python generator.py -o' to generate OEM key \n"
+                       "Use 'python generator.py -e' to generate 11-digit CD key \n"
+                       "Use 'python generator.py -c -o -e' to generate CD, OEM and 11-digit CD key together \n")
